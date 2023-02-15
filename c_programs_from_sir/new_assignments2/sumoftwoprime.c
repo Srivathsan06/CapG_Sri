369002746c40bfe8c45e6a98f8893f2d591339e7
Srivathsan06 <srivathsan06@gmail.com>
2023-02-15 11:10:27 +0530

    improvised code

--- c_programs_from_sir/new_assignments2/sumoftwoprime.c
+++ c_programs_from_sir/new_assignments2/sumoftwoprime.c
@@ -1,14 +1,34 @@
 #include<stdio.h>
 
+int isPrime(int);
+int noOfWays(int);
 int main()
 {
-	int n,i,j,k;
-	int count1=1;
-	int count2=1;
-	int count3=0;
+	int n;
+	int count=0;
 
 	printf("\n\n Enter the number: ");
 	scanf("%d",&n);
+
+	count=noOfWays(n);
+
+	if(count==0)
+	{
+		printf("\n\n No ways!\n\n");
+	}
+	else
+	{
+		printf("\n\n %d ways!!\n\n",count);
+	}
+
+	return 0;
+}
+
+
+int noOfWays(int n)
+{
+	int i,j;
+	int count1=1,count2=1,count3=0;
 
 	for(i=2;i<n;i++)
 	{
@@ -16,36 +36,8 @@ int main()
 		{
 			if(i+j==n)
 			{
-				if(i==0 || i==1)
-				{
-					count1=0;
-				}
-				else
-				{
-					for(k=2;k<i;k++)
-					{	
-						if(i % k == 0)
-						{
-							count1=0;
-							break;
-						}
-					}
-				}
-				if(j==0 || j==1)
-				{
-					count2=0;
-				}
-				else
-				{
-					for(k=2;k<j;k++)
-					{
-						if(j % k ==0)
-						{
-							count2=0;
-							break;
-						}
-					}
-				}
+				count1=isPrime(i);
+				count2=isPrime(j);
 				if(count1==1 && count2==1)
 				{
 					printf("\n\n %d + %d = %d",i,j,n);
@@ -57,17 +49,19 @@ int main()
 		}
 	}
 
-
-	if(count3!=0)
-	{
-		printf("\n\n Ways : %d \n\n",count3);
-	}
-	else
-	{
-		printf("\n\n No ways available!! \n\n");
-	}
-
-	return 0;
-
+	return count3;
 }
 
+int isPrime(int l)
+{
+	int k, m=l, count3=1;
+	for(k=2;k<m;k++)
+	{
+		if(m%k==0)
+		{
+			count3=0;
+			break;
+		}
+	}
+	return count3;
+}
