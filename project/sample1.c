7900752634f5b2ad0862652f9fba33fcbd304654
Srivathsan06 <srivathsan06@gmail.com>
2023-03-22 11:19:19 +0000

    version 2.0

--- project/sample1.c
+++ project/sample1.c
@@ -22,41 +22,42 @@ int main() {
     FILE *fp;
     char line[MAX_LINE_LENGTH];
     char *token;
-    struct User user;
+    struct User *us=NULL;
 
     fp = fopen("data.cdr", "r");
     if (fp == NULL) {
         printf("Error opening file.\n");
-        exit(0.5);
+        exit(0);
     }
-	int count=0;
+	int n=0;
 
-    // read user details from file using fgets and tokenization
     while (fgets(line, MAX_LINE_LENGTH, fp) != NULL) 
 	{
 		printf("\n%s\n",line);
+		n++;
+		struct User *temp=realloc(us,n*sizeof(struct User));
+		us=temp;
 		sleep(1);
-		count++;
-        token = strtok(line, "|");
-        strcpy(user.msisdn, token);
+		token = strtok(line, "|");
+        strcpy(us[n-1].msisdn, token);
         token = strtok(NULL, "|");
-        strcpy(user.opbrandname, token);
+        strcpy(us[n-1].opbrandname, token);
         token = strtok(NULL, "|");
-        strcpy(user.opmmc, token);
+        strcpy(us[n-1].opmmc, token);
         token = strtok(NULL, "|");
-        strcpy(user.caltype, token);
+        strcpy(us[n-1].caltype, token);
         token = strtok(NULL, "|");
-        strcpy(user.dur, token);
+        strcpy(us[n-1].dur, token);
         token = strtok(NULL, "|");
-        strcpy(user.down, token);
+        strcpy(us[n-1].down, token);
         token = strtok(NULL, "|");
-        strcpy(user.upload, token);
+        strcpy(us[n-1].upload, token);
         token = strtok(NULL, "|");
-        strcpy(user.thirdparmsisdn, token);
+        strcpy(us[n-1].thirdparmsisdn, token);
         token = strtok(NULL, "|");
-        strcpy(user.thirdparmmc, token);
-        printf("\nUser %d details:",count);
-        printf("\nMSISDN: %s \nOP BRAND NAME: %s \nOP MMC/MNC: %s\nCALL TYPE: %s \nDURATION: %s\nDOWNLOAD: %s\nUPLOAD: %s\nTHIRD PARTY MSISDN: %s\nTHIRD PARTY OPERATOR MMC/MNC: %s\n", user.msisdn, user.opbrandname, user.opmmc, user.caltype, user.dur, user.down, user.upload, user.thirdparmsisdn, user.thirdparmmc);
+        strcpy(us[n-1].thirdparmmc, token);
+        printf("\nUser %d details:",n);
+        printf("\nMSISDN: %s \nOP BRAND NAME: %s \nOP MMC/MNC: %s\nCALL TYPE: %s \nDURATION: %s\nDOWNLOAD: %s\nUPLOAD: %s\nTHIRD PARTY MSISDN: %s\nTHIRD PARTY OPERATOR MMC/MNC: %s\n", us[n-1].msisdn, us[n-1].opbrandname, us[n-1].opmmc, us[n-1].caltype, us[n-1].dur, us[n-1].down, us[n-1].upload, us[n-1].thirdparmsisdn, us[n-1].thirdparmmc);
    
    }
 
